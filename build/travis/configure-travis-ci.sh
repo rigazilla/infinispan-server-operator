7f055d6266f8707c492a8c30491bf1d9ffbb95c3
Vittorio Rigamonti <vrigamon@redhat.com>
2021-06-16 16:29:31 +0200

    arm tools

--- build/travis/configure-travis-ci.sh
+++ build/travis/configure-travis-ci.sh
@@ -6,8 +6,8 @@ export RUN_SA_OPERATOR=TRUE
 export INITCONTAINER_IMAGE=quay.io/quay/busybox:latest
 export KUBECONFIG=~/kind-kube-config.yaml
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -Lo kind https://kind.sigs.k8s.io/dl/"${KIND_VERSION}"/kind-linux-amd64 && chmod +x kind && sudo mv kind /usr/local/bin/
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/arm64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -Lo kind https://kind.sigs.k8s.io/dl/"${KIND_VERSION}"/kind-linux-arm64 && chmod +x kind && sudo mv kind /usr/local/bin/
 kind create cluster --config kind-config.yaml
 kind get kubeconfig > "${KUBECONFIG}"
 export TESTING_CONTEXT=$(kubectl --insecure-skip-tls-verify config current-context)
